c0ffa4af9b46619b0fb9047c9b7bc7d29a89abf2
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2018-04-09 21:46:50 +0800

    es6 support info

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # PyChakra
-Chakra in Python
+
+Chakra v1.8.2 in Python
+
+Complete ES6 feature support see [https://kangax.github.io/compat-table/es6/](https://kangax.github.io/compat-table/es6/)
 
 run
 ```
--- test/PyChakra.test.cc
+++ test/PyChakra.test.cc
@@ -6,9 +6,7 @@ using namespace std;
 int main(){
     auto runtime = CreateRuntime();
     auto context = CreateContext(runtime);
-    Eval_JS_File(context, "/Users/droiz/Code/personal/PyChakra/test/thirdparty/react.production.min.js");
-
-    auto res = Eval_JS(context, "(()=>React)();");
+    auto res = Eval_JS_File(context, "/Users/droiz/Code/personal/PyChakra/test/js/es6.js");
 
     cout << res;
 
