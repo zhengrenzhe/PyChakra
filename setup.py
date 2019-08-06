edbd2bc3e7bd8578722894434b7e68aea96f4f62
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2019-08-06 23:18:25 +0800

    release v2.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="PyChakra",
-    version="2.1.3",
+    version="2.2.0",
     packages=["PyChakra"],
     package_dir={"PyChakra": "PyChakra"},
     description="Python binding to Microsoft Chakra JavaScript engine",
